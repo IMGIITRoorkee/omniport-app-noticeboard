ef7d28c4db9106949263c594dd05126f3921a465
Mrinal Kumar <kmrinal19@gmail.com>
2022-05-23 01:22:43 +0530

    Update permissions for public notices

--- permissions/public_notices.py
+++ permissions/public_notices.py
@@ -2,17 +2,23 @@ from rest_framework.permissions import BasePermission
 
 from omniport.settings.configuration.base import CONFIGURATION
 
+
 class isPublicInternet(BasePermission):
     """
-    Permission for public notices 
-    on internet.channeli.in
+    Permission for public notices
     """
 
     def has_object_permission(self, request, view, obj, **kwargs):
         is_public = obj.is_public
-        site_id = CONFIGURATION.site.id
+        is_request_from_internet = False
+        ip_address_rings = request.ip_address_rings
 
-        if (site_id>1 and is_public) or site_id<=1:
-            return True
-        else:
+        # Check if the request only comes under the  internet IP address ring
+        if ('internet' in ip_address_rings) and (len(ip_address_rings) <= 1):
+            is_request_from_internet = True
+
+        if len(ip_address_rings) == 0:
             return False
+
+        return is_public or (not is_request_from_internet)
+
